bdf77ccbc29e22e610b37a16b730c3a4a9394b4e
RobertasBr <123648407+RobertasBr@users.noreply.github.com>
2023-02-18 12:30:52 +0000

    Add project files.

---
+++ menuTemplate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace menuTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menuChoice();
+        }
+        static void menuChoice()
+        {
+            Console.WriteLine("MENU: Select items 1-3");
+
+            string choice = Console.ReadLine();
+            int.TryParse(choice, out int choices);
+
+            switch (choices)
+            {
+                case 1:
+                    Console.WriteLine("Item one");
+                    //Method one goes here
+                    break;
+                case 2:
+                    Console.WriteLine("Item two");
+                    //Method two goes here
+                    break;
+                case 3:
+                    Console.WriteLine("Item three");
+                    //Method three goes here, can keep adding cases if needed
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, try again!");
+                    //Error message
+                    break;
+            }
+        }
+    }
+}
